a06b393868c2c42ae9831c5fd47c3bce522465df
Vyjimka <1162340805@qq.com>
2024-04-21 16:57:04 +0800

    Revert "实现drop table"
    This reverts commit 8c03fca10d6a06d7488515e80ca2c0e425262380.

--- src/observer/storage/table/table.h
+++ src/observer/storage/table/table.h
@@ -49,7 +49,7 @@ public:
    */
   RC create(int32_t table_id, const char *path, const char *name, const char *base_dir, int attribute_count,
       const AttrInfoSqlNode attributes[]);
-  RC destroy(const char* dir);
+
   /**
    * 打开一个表
    * @param meta_file 保存表元数据的文件完整路径
